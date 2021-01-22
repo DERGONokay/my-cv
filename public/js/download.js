283f95fde8eab26fb9a9b913059659e00294de8d
DERGON OKAY <dergonokay@gmail.com>
2021-01-22 02:25:38 -0300

    add logs for debug

--- public/js/download.js
+++ public/js/download.js
@@ -2,11 +2,12 @@ $(document).ready(function() {
     $("#download-pdf").click(function() {
         let url = `https://my-curriculum-api.herokuapp.com/api/download/link/pdf`
 
-        fetch(url, {mode: "cors"})
+        fetch(url)
         .then(response => {
             console.log(response)
             console.log(response.body)
             console.log(response.status)
+            console.log(response.text)
         })
 
     })

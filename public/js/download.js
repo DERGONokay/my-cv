bb93a999ad9f482ddd8684a31b03eeb0aec60873
DERGON OKAY <dergonokay@gmail.com>
2021-01-22 02:09:28 -0300

    Add cors mode to fetch

--- public/js/download.js
+++ public/js/download.js
@@ -2,6 +2,18 @@ $(document).ready(function() {
     $("#download-pdf").click(function() {
         let url = `https://my-curriculum-api.herokuapp.com/api/download/link/pdf`
 
+        fetch(url, {mode: "cors"})
+        .then(response => {
+            console.log(response)
+            console.log(response.body)
+            console.log(response.status)
+        })
+
+    })
+
+    $("#download-docx").click(function() {
+        let url = `https://my-curriculum-api.herokuapp.com/api/download/link/docx`
+
         fetch(url)
         .then(response => {
             console.log(response)

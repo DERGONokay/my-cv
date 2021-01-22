5d055752cb95e3c192cfb80b15bba7d55b223931
DERGON OKAY <dergonokay@gmail.com>
2021-01-22 03:56:56 -0300

    KLIN KODE

--- public/js/download.js
+++ public/js/download.js
@@ -1,82 +1,58 @@
 $(document).ready(function() {
     $("#download-pdf").click(function() {
-        let url = `https://my-curriculum-api.herokuapp.com/api/download/link/pdf`
-
-        Swal.fire({
-            title: "Submit your email",
-            text: "This is optional ;)",
-            confirmButtonText: "There you go",
-            showCancelButton: true,
-            cancelButtonText: "Nah",
-            showLoaderOnConfirm: true,
-            input: "email",
-            preConfirm: (email) => {
-                if(email) {
-                    fetch(`https://my-curriculum-api.herokuapp.com/api/download/register`, {
-                        method: "POST",
-                        headers: {
-                            'Accept': 'application/json',
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({email: email, docType: "pdf"})
-                    })
-                }
-            }
-        }).then(_ => {
-            fetch(url)
-            .then(response => {
-                response.json().then(downloadResponse => {
-                    if(downloadResponse.url) {
-                        window.open(downloadResponse.url, "_balnk")
-                    } else {
-                        Swal.fire({
-                            icon: 'error',
-                            title: 'Oops...',
-                            text: 'The download url is not available :('
-                          })
-                    }
-                })
-            })
-        })
-
-
+        download("pdf")
     })
 
     $("#download-docx").click(function() {
-        let url = `https://my-curriculum-api.herokuapp.com/api/download/link/docx`
-
-        Swal.fire({
-            title: "Submit your email",
-            text: "This is optional ;)",
-            confirmButtonText: "There you go",
-            cancelButtonText: "Nah",
-            preConfirm: (email) => {
-                if(email) {
-                    fetch(`https://my-curriculum-api.herokuapp.com/api/download/register`, {
-                        method: "POST",
-                        body: JSON.stringify({email: email, docRype: "docx"})
-                    })
-                }
-            }
-        }).then(_ => {
-            fetch(url)
-            .then(response => {
-                response.json().then(downloadResponse => {
-                    if(downloadResponse.url) {
-                        window.open(downloadResponse.url, "_balnk")
-                    } else {
-                        Swal.fire({
-                            icon: 'error',
-                            title: 'Oops...',
-                            text: 'The download url is not available :('
-                          })
-                    }
-                })
-            })
-        })
-
+        download("docx")
     })
 })
+
+function download(format) {
+
+    let HOST = "my-curriculum-api.herokuapp.com"
+
+    Swal.fire({
+        title: "Submit your email",
+        text: "This is optional ;)",
+        confirmButtonText: "There you go",
+        showCancelButton: true,
+        cancelButtonText: "Nah",
+        showLoaderOnConfirm: true,
+        input: "email",
+        preConfirm: (email) => {
+            if(email) {
+                fetch(`https://${HOST}/api/download/register`, {
+                    method: "POST",
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({email: email, docType: format})
+                })
+            }
+        }
+    }).then(_ => {
+        fetch(`https://${HOST}/api/download/link/${format}`)
+        .then(response => {
+            response.json().then(downloadResponse => {
+                if(downloadResponse.url) {
+                    window.open(downloadResponse.url, "_balnk")
+                } else {
+                    unavailableAlert()
+                }
+            })
+        })
+    })
+}
+
+function unavailableAlert() { 
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'The download url is not available :('
+    })
+}
 
 function DownloadResponse(url) {
     this.url = url

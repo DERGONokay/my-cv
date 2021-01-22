cdede868dd301f2f1d3348753e5339ec89faeb18
DERGON OKAY <dergonokay@gmail.com>
2021-01-22 02:54:28 -0300

    add download registration

--- public/js/download.js
+++ public/js/download.js
@@ -2,24 +2,75 @@ $(document).ready(function() {
     $("#download-pdf").click(function() {
         let url = `https://my-curriculum-api.herokuapp.com/api/download/link/pdf`
 
-        fetch(url)
-        .then(response => {
-            response.json().then(json => {
-                console.log(json)
+        Swal.fire({
+            title: "Submit your email",
+            text: "This is optional ;)",
+            confirmButtonText: "There you go",
+            cancelButtonText: "Nah",
+            preConfirm: (email) => {
+                if(email) {
+                    fetch(`https://my-curriculum-api.herokuapp.com/api/download/register`, {
+                        method: "POST",
+                        body: JSON.stringify({email: email, docRype: "pdf"})
+                    })
+                }
+            }
+        }).then(_ => {
+            fetch(url)
+            .then(response => {
+                response.json().then(downloadResponse => {
+                    if(downloadResponse.url) {
+                        window.location = downloadResponse.url
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'The download url is not available :('
+                          })
+                    }
+                })
             })
         })
+
 
     })
 
     $("#download-docx").click(function() {
         let url = `https://my-curriculum-api.herokuapp.com/api/download/link/docx`
 
-        fetch(url)
-        .then(response => {
-            console.log(response)
-            console.log(response.body)
-            console.log(response.status)
+        Swal.fire({
+            title: "Submit your email",
+            text: "This is optional ;)",
+            confirmButtonText: "There you go",
+            cancelButtonText: "Nah",
+            preConfirm: (email) => {
+                if(email) {
+                    fetch(`https://my-curriculum-api.herokuapp.com/api/download/register`, {
+                        method: "POST",
+                        body: JSON.stringify({email: email, docRype: "docx"})
+                    })
+                }
+            }
+        }).then(_ => {
+            fetch(url)
+            .then(response => {
+                response.json().then(downloadResponse => {
+                    if(downloadResponse.url) {
+                        window.location = downloadResponse.url
+                    } else {
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'The download url is not available :('
+                          })
+                    }
+                })
+            })
         })
 
     })
 })
+
+function DownloadResponse(url) {
+    this.url = url
+}
